d9ca489d16d67a05287ff54eb2c64f6a55989748
mano <spalania@mtu.edu>
2017-02-28 19:38:26 -0500

    recalibrate the threshold values in rand_move.py

--- catkin_ws/src/lab_5_floor_mapper/src/scripts/rand_move.py
+++ catkin_ws/src/lab_5_floor_mapper/src/scripts/rand_move.py
@@ -51,10 +51,10 @@ start_time  =  0
 front_delta = 15
 side_ang    = 30
 side_delta  = 15
-side_thresh = 1.55
+side_thresh = 1
 
 # increment value at each time step
-rate_step = 0.10
+rate_step_l = 0.15
 
 # linear accleration and decceleration
 def smooth_vel(vel_before, vel_final, t_before, t_final, rate):
@@ -123,28 +123,28 @@ def Callback(data):
 
     # All Clear, randomly drive forward with varying turn
     elif (frontAve > 3) and (leftAve > side_thresh) and (rightAve > side_thresh) :
-        angular_min = -1.00 * scale
-        angular_max = 1.00 * scale
+        angular_min = -0.5 * scale
+        angular_max = 0.5 * scale
         linear_min  = 0.50 * scale
-        linear_max  = 1.0 * scale
+        linear_max  = 0.7 * scale
 
     # Close to a wall on one side, turn to side with most time
     else :
         if leftAve > rightAve :
-            angular_min = 0.75 * scale
-            angular_max = 1.0 * scale
+            angular_min = 0.5 * scale
+            angular_max = 0.5 * scale
             linear_min  = 0.25 * scale
             linear_max  = 0.75 * scale
         else :
-            angular_min = -1.0 * scale
-            angular_max = -0.75 * scale
+            angular_min = -0.5 * scale
+            angular_max = -0.5 * scale
             linear_min  = 0.25 * scale
             linear_max  = 0.75 * scale
 
 
 # define setup and run routine
 def setup():
-    global start_time, rate_step, ramp_timeL
+    global start_time, rate_step_l, ramp_timeL, rate_step_a
     start_time = time.time()
 
     # create node for listening to twist messages
@@ -153,7 +153,7 @@ def setup():
     # subscribe to all
     rospy.Subscriber("/scan", LaserScan, Callback)
     # rate = rospy.Rate(user_rate)
-    rate = rospy.Rate(100)
+    rate = rospy.Rate(50)
 
     # publish to cmd_vel of the jackal
     pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
@@ -169,7 +169,7 @@ def setup():
     randLin_n = twist_init.linear.x
     randAng_n = twist_init.angular.z
     # loop
-    while not time.time()-start_time>60:
+    while not time.time()-start_time>600:
         # get a new time stamp
         ramp_timeF = time.time()
         # generate random movement mapping at random interval
@@ -183,20 +183,15 @@ def setup():
 
         # pass linear and angular velocity values to the smooth_vel function
         # untill randLin and randLin_n matches
-        if not(randLin == randLin_n):
-            randLin_n = smooth_vel(randLin_n, randLin, ramp_timeL, ramp_timeF, rate_step)
+        if not(randLin == randLin_n and randLin_n < linear_max):
+            randLin_n = smooth_vel(randLin_n, randLin, ramp_timeL, ramp_timeF, rate_step_l)
         else:
             randLin_n = randLin
-
-        if not(randAng_n == randAng):
-            randAng_n = smooth_vel(randAng_n, randAng, ramp_timeL, ramp_timeF, rate_step)
-        else:
-            randAng_n = randAng
 
         # push Twist msgs
         linear_msg  = Vector3(x=randLin_n, y=float(0.0), z=float(0.0))
 
-        angular_msg = Vector3(x=float(0.0), y=float(0.0), z=randAng_n)
+        angular_msg = Vector3(x=float(0.0), y=float(0.0), z=randAng)
         publish_msg = Twist(linear=linear_msg, angular=angular_msg)
 
         ramp_timeL = ramp_timeF
